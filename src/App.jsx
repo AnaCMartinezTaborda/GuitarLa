af4c10627d9f59e5d24edd992107e7c34015cd89
Ana Martinez <anacmartineztaborda@gmail.com>
2025-09-24 22:30:15 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,26 @@ import Guitar from "./components/Guitar"
 import { db } from "./data/db"
 
 function App() {
-  const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [data] = useState(db)
+  const [cart, setCart] = useState(initialCart)
+
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
+  useEffect(()=> {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   function addToCart(item) {
     const itemExists = cart.findIndex(guitar => guitar.id === item.id)
-    if (itemExists>= 0) { //Existe en el carrito
-      console.log('Ya existe');
+    if (itemExists>= 0 ) { //Existe en el carrito
+      if(cart[itemExists].quantity >= MAX_ITEMS) return
       const updateCart = [...cart]
       updateCart[itemExists].quantity++
       setCart(updateCart)
@@ -18,13 +31,56 @@ function App() {
       item.quantity = 1
       setCart([...cart, item])
     }
-    
+    saveLocalStorage()
   }
+
+  function removeFromCart(id){
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+  
+  function decrementQuantity(id){
+    const updateCart = cart.map(item => {
+      if(item.id === id && item.quantity > MIN_ITEMS){
+        return{
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function increaseQuantity(id){
+    const updateCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS){
+        return{
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function clearCart(){
+    setCart([])
+  }
+
+  function saveLocalStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }
+
 
   return (
     <>
     <Header 
       cart = {cart}
+      removeFromCart = {removeFromCart}
+      decrementQuantity = {decrementQuantity}
+      increaseQuantity = {increaseQuantity}
+      clearCart = {clearCart}
     />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
